0adc841cbe7e3f810952e1db0c32091231757b36
Valentin Buza <valentinbuza@protonmail.ch>
2018-08-30 12:20:04 +0300

    add week 1

---
+++ week_01/Transition.scala
@@ -0,0 +1,30 @@
+import scala.collection.immutable.Queue
+
+case class State(resultQ: Queue[Int], elemC: Option[Int], rest: List[Int])
+
+val lst = List(-7, 1, 5, 2, -4, 3, 0)
+
+val initialState = State(Queue.empty[Int], lst.headOption, lst.tail)
+
+def transition(s: State): State = {
+  println(s)
+  val restHeadOption = s.rest.headOption
+  restHeadOption.map { case restHead =>
+    val nextRest = s.rest.tail
+    val min = scala.math.min(restHead, s.elemC.get)
+    val max = scala.math.max(restHead, s.elemC.get)
+    State(s.resultQ.enqueue(min), Some(max), nextRest)
+  }.getOrElse {
+    val nextResultQ = s.elemC.map { case el => s.resultQ.enqueue(el) }.getOrElse(s.resultQ)
+    State(nextResultQ, None, List.empty[Int])
+  }
+}
+
+val afterFirstBubble = {
+  Stream
+    .iterate(initialState)(transition)
+    .dropWhile { case State(_, elemC, _) => elemC.nonEmpty }
+    .head
+    .resultQ
+    .toList
+}
